a8db01dc6cd8495d44f58c8225cd36a6ccad4414
dmitry4567 <dimon2003pp@gmail.com>
2024-06-01 17:27:12 +0300

    add refresh token

---
+++ src/token/entities/token.entity.ts
@@ -0,0 +1,14 @@
+import { UserEnitity } from 'src/user/entities/user.entity';
+import { Column, Entity, ManyToOne, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('token')
+export class TokenEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => UserEnitity, (user) => user.token)
+  user: UserEnitity;
+
+  @Column()
+  refresh_token: string;
+}
